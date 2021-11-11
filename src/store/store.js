115dc35b1073f22bd35ad92e9b4bc04856b47576
Alejandro Crespo Mejías <alejandroc311@gmail.com>
2021-11-10 22:10:21 -0500

    refactor: Move combineReducers() to rootReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,11 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userSlice from "./slicers/userSlice";
 
-const rootReducer = {
+const rootReducer = combineReducers({
     userSlice
-}
+})
 export default configureStore(
     {
-        reducer: combineReducers(rootReducer)
+        reducer: rootReducer
     }
 )
