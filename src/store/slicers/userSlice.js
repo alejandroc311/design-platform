e3c49008066d3cb7fce976abd8d5ee59ef19127c
Alejandro Crespo Mejías <alejandroc311@gmail.com>
2021-11-12 14:08:26 -0500

    chore: Clean up and format code

--- src/store/slicers/userSlice.js
+++ src/store/slicers/userSlice.js
@@ -1,5 +1,4 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
 
 export const getUser = createAsyncThunk("user/getUser", async () => {
     const user = await fetch(
@@ -7,7 +6,7 @@ export const getUser = createAsyncThunk("user/getUser", async () => {
         {
             method: "GET"
         })
-    .then( res => res.json());
+        .then( res => res.json());
     console.log(user);
     return user;
 });
@@ -23,14 +22,11 @@ const userSlice = createSlice({
         
 
     }, 
-    //This part of the slice reducer handles thunk "getUser()." This thunk was genereated by createAsyncThunk.
-    //createAsyncThunk also generates "fulfilled," "idle," "error," etc. action types that will be dispatched 
-    //during the thunk's lifecycle. 
-    
-    //extraReducers is a createSlice parameter that takes a reducer builder as its parameter. The builder can take an 
-    //action type as its first parameter and then a callback that functions as a reducer which returns state.
-    //Since createAsyncThunk generates and dispatches a "fulfilled" action type when the user/getUser action
-    //type is dispatched (when getUser is called), we can listen for "getUser.fulfilled" inside the builder
+    //extraReducers is a createSlice parameter that takes a builder as its parameter. The builder can 
+    //take an action type and a callback that functions as a reducer as its parameters.
+    // Since createAsyncThunk generates and dispatches a "fulfilled" action type when the "user/getUser" 
+    //action type is dispatched (when getUser is called), we can listen for "getUser.fulfilled" inside 
+    //the builder.
     extraReducers: (builder) => {
         builder.addCase(getUser.fulfilled, (state = {}, action) => {
             let {userid} = action.payload; 
