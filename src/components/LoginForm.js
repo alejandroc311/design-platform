c5c5b91a60b36e0e4822d7c404b38be38557e90d
Alejandro Crespo Mejías <alejandroc311@gmail.com>
2021-12-28 18:13:14 -0500

    feat: Add navigation to "Signup" Page.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import EmailInput from "./EmailInput";
 import PasswordInput from "./PasswordInput";
 function LoginForm({onSubmit}){
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
     function handleInput(value, name){
         name == "email-input" ? setEmail(value) : setPassword(value);
     }
@@ -19,6 +21,8 @@ function LoginForm({onSubmit}){
             <EmailInput userInput={email} onChange={handleInput}/>
             <PasswordInput userInput={password} onChange={handleInput}/>
             <button className="submit-button" type="submit">Sign In</button>
+            <h3>Or create an account!</h3>
+            <button onClick={() => navigate("/signup")}>Sign Up</button>
         </form>
     );
 }
